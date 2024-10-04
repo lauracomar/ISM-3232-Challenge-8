d16b074b948356f0a608da3b41f325f19a56603d
Laura Comar <lautmcomar@gmail.com>
2024-10-04 17:59:22 -0400

    Create Department class to manage employees and calculate salary

--- employee_management.js
+++ employee_management.js
@@ -5,8 +5,23 @@ class Employee { // initiate class
         this.salary = salary;
         this.position = position;
         this.department = department;
-    };// using this to assign value to parameter
+    };// using "this" to assign value to parameter
     getDetails() {
         return `Employee Name: ${this.name}, Salary: $${this.salary}, Position: ${this.position}`;
     }; // return string with employees name, salary, position
 };
+
+// Task 2: Create a Department Class
+class Departments { // creating class
+    constructor(name) { // initiate department
+        this.name = name;
+        this.employees = []; // empty array for employees in departments
+    }; // use "this" to assign value to parameter (department details)
+    addEmployee(employee) {
+        this.employees.push(employee); // use push to add given employee to the empty employees array
+    };
+    getDepartmentSalary() { // calculate total salary of employees in department
+        return this.employees.reduce((total, employee) => total + employee.salary, 0); // use "reduce" to go over each employee and get salary
+    };
+};
+
