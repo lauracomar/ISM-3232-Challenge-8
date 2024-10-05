1b31725c7f269e9598a174c9d6f112c502635813
Laura Comar <lautmcomar@gmail.com>
2024-10-05 15:55:40 -0400

    Create and manage departments and employees (+ corrected error in calculateTotalSalaryWithBonus function)

--- employee_management.js
+++ employee_management.js
@@ -28,7 +28,8 @@ class Departments { // creating class
         return this.employees.reduce((total, employee) => { // use reduce to go ogver employees array and get total salary
             if (employee instanceof Manager) { // check if employee is manager 
                 return total + employee.salary + employee.bonus; // if employee is manager add bonus to salary
-            };
+            }
+            else { return total + employee.salary }; // instructions if employee is not manager 
         }, 0);
     };
 };
@@ -43,3 +44,28 @@ class Manager extends Employee { // new manager class that extends within employ
         return `Employee Name: ${this.name}, Salary: $${this.salary}, Position: ${this.position}, Bonus: $${this.bonus}`;
     }; // add bonus to getDetails string
 };
+
+//Task 5: Create and Manage Departments and Employees
+// creating depaterments
+const finance = new Departments(`Finance`);
+const sales = new Departments(`Sales`);
+// creating employees/ managers
+const laura = new Employee(`Laura`, 50000, `Analyst`, `Finance`);
+const bruna = new Manager(`Bruna`, 100000, `Manager`, `Finance`, 30000);
+const beatriz = new Employee(`Beatriz`, 60000, `Sales Rep`, `Sales`);
+const eduardo = new Manager(`Eduardo`, 110000, `Manager`, `Sales`, 45000);
+// console log employee details
+console.log(laura.getDetails()); // should display details above
+console.log(bruna.getDetails());
+console.log(beatriz.getDetails());
+console.log(eduardo.getDetails());
+// add new employees to respective departments
+finance.addEmployee(laura);
+finance.addEmployee(bruna);
+sales.addEmployee(beatriz);
+sales.addEmployee(eduardo);
+// console log to create output
+console.log(`Finance Department Salary:$${finance.getDepartmentSalary()}`);
+console.log(`Sales Department Salary: $${sales.getDepartmentSalary()}`);
+console.log(`Finance Salary with bonus: $${finance.calculateTotalSalaryWithBonus()}`);
+console.log(`Sales Salary with bonus: $${sales.calculateTotalSalaryWithBonus()}`);
